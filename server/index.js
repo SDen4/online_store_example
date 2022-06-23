7786dc97f42e14e902a5ac51698da39a55a850d2
SDen4 <asw325@yandex.ru>
2022-06-23 11:09:03 +0500

    server: basic server started

--- server/index.js
+++ server/index.js
@@ -1,1 +1,7 @@
-console.log('server!');
+const express = require('express');
+
+const PORT = 5000;
+
+const app = express();
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}.`));
