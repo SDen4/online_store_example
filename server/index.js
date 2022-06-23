718b93a12d23e89eafb3315cb2937288ab8fcb32
SDen4 <asw325@yandex.ru>
2022-06-23 20:20:06 +0500

    server: connect to db

--- server/index.js
+++ server/index.js
@@ -1,8 +1,19 @@
-const express = require('express');
 require('dotenv').config();
+const express = require('express');
+const sequelize = require('./db');
 
 const PORT = process.env.PORT;
 
 const app = express();
 
-app.listen(PORT, () => console.log(`Server started on port ${PORT}.`));
+const start = async () => {
+  try {
+    await sequelize.authenticate();
+    await sequelize.sync();
+    app.listen(PORT, () => console.log(`Server started on port ${PORT}.`));
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
