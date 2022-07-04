6487eba8db21a35b086bd2b8064f60d06868dec5
SDen4 <asw325@yandex.ru>
2022-07-04 09:38:53 +0500

    client: logic to auth component, useLocation example

--- client/src/pages/Auth/Auth.tsx
+++ client/src/pages/Auth/Auth.tsx
@@ -1,19 +1,22 @@
 import React from 'react';
 import { Button, Card, Container, Form } from 'react-bootstrap';
-import { NavLink } from 'react-router-dom';
+import { NavLink, useLocation } from 'react-router-dom';
 
-import { REGISTRATION_ROUTE } from '../../utils/constants';
+import { LOGIN_ROUTE, REGISTRATION_ROUTE } from '../../utils/constants';
 
 import styles from './styles.module.css';
 
 const Auth = () => {
+  const location = useLocation();
+  const isLogin = location.pathname === LOGIN_ROUTE;
+
   return (
     <Container
       className="d-flex justify-content-center align-items-center"
       style={{ height: window.innerHeight - 55 }}
     >
       <Card style={{ width: 600 }} className="p-5">
-        <h2 className="m-auto">Authorization</h2>
+        <h2 className="m-auto">{isLogin ? 'Authorization' : 'Registration'}</h2>
         <Form className="d-flex flex-column">
           <Form.Control className="mt-3" placeholder="Enter email" />
           <Form.Control
@@ -23,11 +26,19 @@ const Auth = () => {
           />
         </Form>
         <div className={styles.btnsWrapper}>
-          <div>
-            Don't have an account?{' '}
-            <NavLink to={REGISTRATION_ROUTE}>Registration</NavLink>{' '}
-          </div>
-          <Button variant={'outline-success'}>Auth</Button>
+          {isLogin ? (
+            <div>
+              Don't have an account?{' '}
+              <NavLink to={REGISTRATION_ROUTE}>Registration</NavLink>{' '}
+            </div>
+          ) : (
+            <div>
+              Have an account? <NavLink to={LOGIN_ROUTE}>Login</NavLink>{' '}
+            </div>
+          )}
+          <Button variant={'outline-success'}>
+            {isLogin ? 'Login' : 'Registration'}
+          </Button>
         </div>
       </Card>
     </Container>
