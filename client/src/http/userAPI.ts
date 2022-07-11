905b39439db129f74869378236f84cc168d00c31
SDen4 <asw325@yandex.ru>
2022-07-11 16:18:06 +0500

    client: registration request logic

--- client/src/http/userAPI.ts
+++ client/src/http/userAPI.ts
@@ -1,10 +1,23 @@
 import { $authHost, $host } from './index';
 
 export const registration = async (email: string, password: string) => {
-  const response = await $host.post('api/auth/registration', {
+  const response = await $host.post('api/user/registration', {
     email,
     password,
     role: 'ADMIN',
   });
+  return response;
+};
+
+export const login = async (email: string, password: string) => {
+  const response = await $host.post('api/user/login', {
+    email,
+    password,
+  });
+  return response;
+};
+
+export const check = async () => {
+  const response = await $host.post('api/auth/registration', {});
   return response;
 };
--- client/src/pages/Auth/Auth.tsx
+++ client/src/pages/Auth/Auth.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Button, Card, Container, Form } from 'react-bootstrap';
 import { NavLink, useLocation } from 'react-router-dom';
+import { login, registration } from '../../http/userAPI';
 
 import { LOGIN_ROUTE, REGISTRATION_ROUTE } from '../../utils/constants';
 
@@ -10,6 +11,18 @@ const Auth = () => {
   const location = useLocation();
   const isLogin = location.pathname === LOGIN_ROUTE;
 
+  const [email, setEmail] = useState<string>('');
+  const [password, setPassword] = useState<string>('');
+
+  const click = async () => {
+    if (isLogin) {
+      const response = await login(email, password);
+    } else {
+      const response = await registration(email, password);
+      console.log(response);
+    }
+  };
+
   return (
     <Container
       className="d-flex justify-content-center align-items-center"
@@ -18,11 +31,18 @@ const Auth = () => {
       <Card style={{ width: 600 }} className="p-5">
         <h2 className="m-auto">{isLogin ? 'Authorization' : 'Registration'}</h2>
         <Form className="d-flex flex-column">
-          <Form.Control className="mt-3" placeholder="Enter email" />
+          <Form.Control
+            className="mt-3"
+            placeholder="Enter email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
           <Form.Control
             className="mt-3"
             type="password"
             placeholder="Enter password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
           />
         </Form>
         <div className={styles.btnsWrapper}>
@@ -36,7 +56,7 @@ const Auth = () => {
               Have an account? <NavLink to={LOGIN_ROUTE}>Login</NavLink>{' '}
             </div>
           )}
-          <Button variant={'outline-success'}>
+          <Button variant={'outline-success'} onClick={click}>
             {isLogin ? 'Login' : 'Registration'}
           </Button>
         </div>
